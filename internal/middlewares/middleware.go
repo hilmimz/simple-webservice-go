91ec7feb8700a0fc85df382889e602b3b10ede35
Hilmi Muzhaffar <hilmimuzhaffar15@gmail.com>
2025-12-09 11:40:42 +0700

    add getonly middleware

---
+++ internal/middlewares/middleware.go
@@ -0,0 +1,13 @@
+package middlewares
+
+import "net/http"
+
+func OnlyGET(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		next(w, r)
+	}
+}
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"simple-webservice/internal/handlers"
+	"simple-webservice/internal/middlewares"
 	"simple-webservice/internal/services"
 )
 
@@ -16,8 +17,8 @@ func main() {
 
 	routerHandler := handlers.NewRouterHandler(routerService)
 
-	http.HandleFunc("/api/uptime/avg", routerHandler.AvgUptime)
-	http.HandleFunc("/api/uptime/availability", routerHandler.Availability)
+	http.HandleFunc("/api/uptime/avg", middlewares.OnlyGET(routerHandler.AvgUptime))
+	http.HandleFunc("/api/uptime/availability", middlewares.OnlyGET(routerHandler.Availability))
 
 	log.Println("Server running on http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
